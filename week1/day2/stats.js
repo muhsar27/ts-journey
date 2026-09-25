2d34149797d2addd2a460cb9844937dc1016cd0a
msarumi <muhsar4321@gmail.com>
2026-09-25 10:03:08 +0100

    Add HTML structure and JavaScript functions for guessing game statistics

--- week1/day2/stats.js
+++ week1/day2/stats.js
@@ -1,5 +1,34 @@
-function findMax(numbers){
-    arguments.length
+function findMax(numbers) {
+  let max = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
 }
-findMin(numbers)
-findAverage(numbers)
+function findMin(numbers) {
+  let min = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < min) {
+      min = numbers[i];
+    }
+  }
+  return min;
+}
+
+function findAverage(numbers) {
+  let avg,
+    sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  avg = sum / numbers.length;
+  return avg;
+}
+
+const nums = [12, 5, 88, 3, 45, 88, -7];
+console.log(findMax(nums)); // 88
+console.log(findMin(nums)); // -7
+console.log(findAverage(nums));
